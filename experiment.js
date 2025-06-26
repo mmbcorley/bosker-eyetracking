e2833f1384fac40e8e6dfdfcfdb51271f49493cc
Martin Corley <Martin.Corley@ed.ac.uk>
2025-06-26 16:54:58 +0100

    new plugin

--- experiment.js
+++ experiment.js
@@ -349,7 +349,7 @@ const fixation = {
 };
 
 const stimulus = {
-    type: jsPsychAudioBettyResponse,
+    type: jsPsychWebGazerTwoItem,
     stimulus: 'audio/sound_check.ogg',
     trial_ends_after_audio: false,
     css_classes: ['trial_content'],
