986d13a99ae32b0b7be785856320f9b0a565c506
Martin Corley <Martin.Corley@ed.ac.uk>
2025-06-27 13:29:38 +0100

    new audio

--- plugins/jspsych-web-gazer-two-item.js
+++ plugins/jspsych-web-gazer-two-item.js
@@ -98,169 +98,210 @@ var jsPsychWebGazerTwoItem = (function (jspsych) {
     static {
       this.info = info;
     }
-    trial(display_element, trial) {
-      const context = this.jsPsych.pluginAPI.audioContext();
-      let audio;
-      let left_bbox = null;
-      let right_bbox = null;
-      const response = {
-        rt: null,
-        button: null
-      };
-      let startTime;
-      let audioEndTime = null;
-      let audioStartTime = null;
-      const round = (number, digits = 2) => {
-        const power_of_10 = Math.pow(10, digits);
-        return Math.round(number * power_of_10) / power_of_10;
-      };
-      const get_stimuli_name = (image_path) => {
-        if (typeof image_path !== "string") return null;
-        const path_pieces = image_path.split("/");
-        return path_pieces[path_pieces.length - 1];
-      };
-      const end_trial = () => {
-        this.jsPsych.pluginAPI.clearAllTimeouts();
-        if (audio) {
-          if (context) {
-            audio.stop();
-          } else {
-            audio.pause();
-          }
-          audio.removeEventListener("ended", end_trial);
-          audio.removeEventListener("ended", enable_buttons);
-          audio.removeEventListener("ended", on_audio_ended);
-          audio.removeEventListener("ended", reveal_mouse);
-        }
-        var trial_data = {
-          rt: response.rt,
-          stimulus: get_stimuli_name(trial.stimulus),
-          left_img: get_stimuli_name(trial.choices[0]),
-          right_img: get_stimuli_name(trial.choices[1]),
-          choice: response.button,
-          audio_onset: audioStartTime,
-          audio_offset: audioEndTime,
-          l_img_top: left_bbox ? round(left_bbox.top) : null,
-          l_img_bottom: left_bbox ? round(left_bbox.bottom) : null,
-          l_img_left: left_bbox ? round(left_bbox.left) : null,
-          l_img_right: left_bbox ? round(left_bbox.right) : null,
-          r_img_top: right_bbox ? round(right_bbox.top) : null,
-          r_img_bottom: right_bbox ? round(right_bbox.bottom) : null,
-          r_img_left: right_bbox ? round(right_bbox.left) : null,
-          r_img_right: right_bbox ? round(right_bbox.right) : null
-        };
-        display_element.innerHTML = "";
-        this.jsPsych.finishTrial(trial_data);
-      };
-      const button_response = (e) => {
-        const choice = e.currentTarget.getAttribute("data-choice");
-        after_response(choice);
-      };
-      const disable_buttons = () => {
-        const btns = document.querySelectorAll(".jspsych-audio-betty-response-button");
-        btns.forEach((btn) => {
-          btn.removeEventListener("click", button_response);
-          const button_element = btn.querySelector("button");
-          if (button_element) button_element.disabled = true;
-        });
-      };
-      const enable_buttons = () => {
-        const btns = document.querySelectorAll(".jspsych-audio-betty-response-button");
-        btns.forEach((btn) => {
-          btn.addEventListener("click", button_response);
-          const button_element = btn.querySelector("button");
-          if (button_element) button_element.disabled = false;
-        });
-      };
-      const after_response = (choice) => {
-        const endTime = performance.now();
-        const rt = round(endTime - startTime, 3);
-        response.button = choice;
-        response.rt = rt;
-        disable_buttons();
-        if (trial.response_ends_trial) {
-          end_trial();
-        }
-      };
-      const on_audio_ended = () => {
-        audioEndTime = round(performance.now() - startTime, 3);
-        left_bbox = document.querySelector("#button-left").getBoundingClientRect();
-        right_bbox = document.querySelector("#button-right").getBoundingClientRect();
-      };
-      const reveal_mouse = () => {
-        document.documentElement.style.cursor = "auto";
-      };
-      const setupTrial = () => {
-        if (trial.trial_ends_after_audio) {
-          audio.addEventListener("ended", end_trial);
-        }
-        if (!trial.response_allowed_while_playing && !trial.trial_ends_after_audio) {
-          audio.addEventListener("ended", enable_buttons);
-        }
-        if (trial.hide_mouse_during_audio) {
-          document.documentElement.style.cursor = "none";
-          audio.addEventListener("ended", reveal_mouse);
-        }
-        let buttons = [];
-        if (Array.isArray(trial.button_html) && trial.button_html.length === trial.choices.length) {
-          buttons = trial.button_html;
-        } else {
-          for (let i = 0; i < trial.choices.length; i++) {
-            buttons.push(trial.button_html);
-          }
-        }
-        const left_img = buttons[0].replace(/%choice%/g, trial.choices[0]);
-        const right_img = buttons[1].replace(/%choice%/g, trial.choices[1]);
-        let html = `
-        <div id="jspsych-audio-betty-response-btngroup" class="container-fluid" style="height: 100%">
-          <div class="row" style="height: 100%">
-            <div class="col-md-5 align-self-center">
-              <div class="jspsych-audio-betty-response-button" style="display: inline-block; margin: ${trial.margin_vertical} ${trial.margin_horizontal};" id="button-left" data-choice="left">${left_img}</div>
-            </div>
-            <div class="col-md-2"></div>
-            <div class="col-md-5 align-self-center">
-              <div class="jspsych-audio-betty-response-button" style="display: inline-block; margin: ${trial.margin_vertical} ${trial.margin_horizontal};" id="button-right" data-choice="right">${right_img}</div>
-            </div>
-          </div>
-        </div>`;
-        if (trial.prompt !== null) {
-          html += trial.prompt;
-        }
-        display_element.innerHTML = html;
-        if (trial.response_allowed_while_playing) {
-          enable_buttons();
-        } else {
-          disable_buttons();
-        }
-        audio.addEventListener("ended", on_audio_ended);
-        startTime = performance.now();
-        audioStartTime = round(performance.now() - startTime, 3);
-        if (context) {
-          context.resume();
-          audio.start(context.currentTime);
-        } else {
-          audio.play();
-        }
-        if (trial.trial_duration !== null) {
-          this.jsPsych.pluginAPI.setTimeout(end_trial, trial.trial_duration);
-        }
-      };
-      this.jsPsych.pluginAPI.getAudioBuffer(trial.stimulus).then((buffer) => {
-        if (context) {
-          const source = context.createBufferSource();
-          source.buffer = buffer;
-          source.connect(context.destination);
-          audio = source;
-        } else {
-          audio = buffer;
-          audio.currentTime = 0;
-        }
-        setupTrial();
-      }).catch((err) => {
-        console.error(`Failed to load audio file "${trial.stimulus}".`, err);
-        end_trial();
-      });
+ // PASTE THIS ENTIRE METHOD INTO YOUR PLUGIN'S CLASS
+trial(display_element, trial) {
+  // --- A FEW DEFINITIONS --- //
+
+  // The audio object will be stored here
+  let audio;
+
+  // Bounding box data
+  let left_bbox = null;
+  let right_bbox = null;
+
+  // Response data
+  const response = {
+    rt: null,
+    button: null,
+  };
+
+  // Timing data
+  let startTime;
+  let audioEndTime = null;
+  let audioStartTime = null;
+
+  // Helper functions
+  const round = (number, digits = 2) => {
+    const power_of_10 = Math.pow(10, digits);
+    return Math.round(number * power_of_10) / power_of_10;
+  };
+
+  const get_stimuli_name = (image_path) => {
+    if (typeof image_path !== "string") return null;
+    const path_pieces = image_path.split("/");
+    return path_pieces[path_pieces.length - 1];
+  };
+
+  // --- TRIAL FUNCTIONS --- //
+
+  const end_trial = () => {
+    this.jsPsych.pluginAPI.clearAllTimeouts();
+
+    // Kill any remaining audio
+    if (audio) {
+      audio.pause();
+      // Remove all event listeners
+      audio.removeEventListener("ended", end_trial);
+      audio.removeEventListener("ended", enable_buttons);
+      audio.removeEventListener("ended", on_audio_ended);
+      audio.removeEventListener("ended", reveal_mouse);
     }
+
+    // Gather data
+    var trial_data = {
+      rt: response.rt,
+      stimulus: get_stimuli_name(trial.stimulus),
+      left_img: get_stimuli_name(trial.choices[0]),
+      right_img: get_stimuli_name(trial.choices[1]),
+      choice: response.button,
+      audio_onset: audioStartTime,
+      audio_offset: audioEndTime,
+      l_img_top: left_bbox ? round(left_bbox.top) : null,
+      l_img_bottom: left_bbox ? round(left_bbox.bottom) : null,
+      l_img_left: left_bbox ? round(left_bbox.left) : null,
+      l_img_right: left_bbox ? round(left_bbox.right) : null,
+      r_img_top: right_bbox ? round(right_bbox.top) : null,
+      r_img_bottom: right_bbox ? round(right_bbox.bottom) : null,
+      r_img_left: right_bbox ? round(right_bbox.left) : null,
+      r_img_right: right_bbox ? round(right_bbox.right) : null,
+    };
+
+    // Clear the display and finish the trial
+    display_element.innerHTML = "";
+    this.jsPsych.finishTrial(trial_data);
+  };
+
+  const button_response = (e) => {
+    const choice = e.currentTarget.getAttribute("data-choice");
+    after_response(choice);
+  };
+
+  const disable_buttons = () => {
+    const btns = document.querySelectorAll(
+      ".jspsych-audio-betty-response-button"
+    );
+    btns.forEach((btn) => {
+      btn.removeEventListener("click", button_response);
+      const button_element = btn.querySelector("button");
+      if (button_element) button_element.disabled = true;
+    });
+  };
+
+  const enable_buttons = () => {
+    const btns = document.querySelectorAll(
+      ".jspsych-audio-betty-response-button"
+    );
+    btns.forEach((btn) => {
+      btn.addEventListener("click", button_response);
+      const button_element = btn.querySelector("button");
+      if (button_element) button_element.disabled = false;
+    });
+  };
+
+  const after_response = (choice) => {
+    const endTime = performance.now();
+    const rt = round(endTime - startTime, 3);
+    response.button = choice;
+    response.rt = rt;
+    disable_buttons();
+
+    if (trial.response_ends_trial) {
+      end_trial();
+    }
+  };
+
+  const on_audio_ended = () => {
+    audioEndTime = round(performance.now() - startTime, 3);
+    left_bbox = document.querySelector("#button-left").getBoundingClientRect();
+    right_bbox = document
+      .querySelector("#button-right")
+      .getBoundingClientRect();
+  };
+
+  const reveal_mouse = () => {
+    document.documentElement.style.cursor = "auto";
+  };
+
+  const setupTrial = () => {
+    // Attach event listeners to audio object
+    audio.addEventListener("ended", on_audio_ended);
+    if (trial.trial_ends_after_audio) {
+      audio.addEventListener("ended", end_trial);
+    }
+    if (
+      !trial.response_allowed_while_playing &&
+      !trial.trial_ends_after_audio
+    ) {
+      audio.addEventListener("ended", enable_buttons);
+    }
+    if (trial.hide_mouse_during_audio) {
+      document.documentElement.style.cursor = "none";
+      audio.addEventListener("ended", reveal_mouse);
+    }
+
+    // --- DISPLAY HTML --- //
+    let buttons = [];
+    if (
+      Array.isArray(trial.button_html) &&
+      trial.button_html.length === trial.choices.length
+    ) {
+      buttons = trial.button_html;
+    } else {
+      for (let i = 0; i < trial.choices.length; i++) {
+        buttons.push(trial.button_html);
+      }
+    }
+    const left_img = buttons[0].replace(/%choice%/g, trial.choices[0]);
+    const right_img = buttons[1].replace(/%choice%/g, trial.choices[1]);
+
+    let html = `
+    <div id="jspsych-audio-betty-response-btngroup" class="container-fluid" style="height: 100%">
+      <div class="row" style="height: 100%">
+        <div class="col-md-5 align-self-center">
+          <div class="jspsych-audio-betty-response-button" style="display: inline-block; margin: ${trial.margin_vertical} ${trial.margin_horizontal};" id="button-left" data-choice="left">${left_img}</div>
+        </div>
+        <div class="col-md-2"></div>
+        <div class="col-md-5 align-self-center">
+          <div class="jspsych-audio-betty-response-button" style="display: inline-block; margin: ${trial.margin_vertical} ${trial.margin_horizontal};" id="button-right" data-choice="right">${right_img}</div>
+        </div>
+      </div>
+    </div>`;
+
+    if (trial.prompt !== null) {
+      html += trial.prompt;
+    }
+    display_element.innerHTML = html;
+
+    // Enable or disable buttons based on trial parameters
+    if (trial.response_allowed_while_playing) {
+      enable_buttons();
+    } else {
+      disable_buttons();
+    }
+    
+    // --- START TIMING AND AUDIO --- //
+    startTime = performance.now();
+    audioStartTime = round(performance.now() - startTime, 3);
+    
+    // Play the audio
+    audio.play();
+
+    // Set up end-of-trial timeout if one is defined
+    if (trial.trial_duration !== null) {
+      this.jsPsych.pluginAPI.setTimeout(end_trial, trial.trial_duration);
+    }
+  };
+
+
+  // --- MAIN EXECUTION LOGIC --- //
+
+  // Use the v8 API to get the audio player
+  audio = this.jsPsych.pluginAPI.getAudioPlayer(trial.stimulus);
+
+  // Once the audio is ready, set up the trial
+  setupTrial();
+}   
+      
   }
 
   return WebGazerTwoItemPlugin;
