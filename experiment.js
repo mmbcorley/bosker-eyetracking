7bf2a9c5aead4044288d68516202a8189e7b44a1
Martin Corley <Martin.Corley@ed.ac.uk>
2025-07-01 15:15:31 +0100

    random LR

--- experiment.js
+++ experiment.js
@@ -55,9 +55,11 @@ function parseData(data,language,group) {
 	    line_data.target_posn = ['L','R'][jsPsych.sampleBernoulli(0.5)];
         }
 	const dashlang='_' + language;
+	console.log(line_data.group,dashlang);
         if (line_data.group == group &&
 	    line_data.audio.includes(dashlang)) {
             all_data.push(line_data);
+	    console.log('thisone');
 	}
     }
 
