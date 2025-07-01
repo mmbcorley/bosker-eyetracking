afbcd166acbfe4759d14552736afe63336687bdf
Martin Corley <Martin.Corley@ed.ac.uk>
2025-07-01 16:10:51 +0100

    simple preload

--- experiment.js
+++ experiment.js
@@ -100,13 +100,13 @@ console.log(all_data);
 
 var image_files = new Set();
 for (var i = 0; i < all_data.length; i++) {
-    image_files.add(all_trials[i].left);
-    image_files.add(all_trials[i].right);
+    image_files.add(all_data[i].left);
+    image_files.add(all_data[i].right);
 }
 
 var audio_files = new Set();
 for (var i = 0; i < all_data.length; i++) {
-        audio_files.add(all_trials[i].audio);
+        audio_files.add(all_data[i].audio);
 }
 
 var preload = {
